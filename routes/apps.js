a0ad0434c4f31c4a93e521ec0f4eed1f51f1ccdc
jminkkk <minl741@naver.com>
2022-07-03 21:39:46 +0900

    feat 라우트 설정

--- routes/apps.js
+++ routes/apps.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
+const { Mbti } = require('../models');
 
-router.get('/know', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.get('/know', async(req, res, next) => {
+    try {
+        const mbti = await Mbti.findall();
+        res.json(mbti);
+    } catch (err){
+        console.err(err);
+        next(err);
+    }
 });
 
+
 router.get('/close', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+ //front
 });
 
 module.exports = router;
--- routes/closes.js
+++ routes/closes.js
@@ -1,24 +1,61 @@
 var express = require('express');
 var router = express.Router();
+const { User } = require('../models');
+const { Sinseo } = require('../models');
+const { Balance } = require('../models');
 
-router.get('/sinseo', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.get('/sinseo', async(req, res, next) => {
+    try {
+        const sinseo = await Sinseo.findall();
+        res.json(sinseo);
+    } catch (err){
+        console.err(err);
+        next(err);
+    }
 });
 
 router.get('/balance', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    try{
+        const balance = await Balance.findall();
+        res.json(balance);
+    } catch(err){
+        console.error(err);
+        next(err);
+    }
 });
 
 router.route('/users')
-    .get(function(req, next){
-    res.send('sdf');
-})
-    .post((req, res, next) => {
-        res.send('sfd');
+    .get(function(req, res, next){ 
+        try{
+            const user = await User.findall({
+                attributes: ['username', 'score'],
+                order: [['score', 'DESC']],
+                limit: 10
+              });
+              res.json(user);                             
+            } catch(err){
+            console.error(err);
+            next(err);
+        }
+
+    })
+    .post((req, res, next) => { 
+        try{
+            const user = await User.create({
+                category : req.body.category,
+                username : req.body.username,
+                score : req.body.score,
+            });
+            console.log(user);
+            res.status(201).json(user);
+        } catch(err){
+            console.error(err);
+            next(err);
+        }
 });
 
-router.get('/sinseo', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.get('/moregame', function(req, res, next) {
+   //front
 });
 
 module.exports = router;
